b96321c703fcdb0201218ff66b990abf4215af65
Rikhil2 <91702512+Rikhil2@users.noreply.github.com>
2022-07-24 17:47:30 -0400

    Update DemoBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DemoBot.java
@@ -8,24 +8,28 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @Autonomous
 public class DemoBot extends LinearOpMode { //drives forward 10 inches, strafes 10 inches left, drives back 10 inches, strafes 10 inches right - even if distances are wrong, it should about make a good square
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode() throws InterruptedException {//test
         DcMotor frontRight, frontLeft, backRight, backLeft;
         frontRight = hardwareMap.dcMotor.get("front right");
         frontLeft = hardwareMap.dcMotor.get("front left");
         backRight = hardwareMap.dcMotor.get("back right");
         backLeft = hardwareMap.dcMotor.get("back left");
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        int targetValue = Math.round(((1120*10)/(4*((float)Math.PI)))*(float)(Math.pow(2, 0.5)));
+        int targetValue = Math.round(((1120*20)/(4*((float)Math.PI)))*(float)(Math.pow(2, 0.5)));
+
+        waitForStart();
 
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        while (backLeft.getCurrentPosition() < targetValue) {
+        while (Math.abs(backLeft.getCurrentPosition()) < targetValue) {
             frontRight.setPower(0.5);
             frontLeft.setPower(0.5);
             backRight.setPower(0.5);
             backLeft.setPower(0.5);
-            telemetry.addData("current position: ", backLeft.getCurrentPosition());
+            telemetry.addData("current position: ", Math.abs(backLeft.getCurrentPosition()));
             telemetry.addData("percent done: ", ((backLeft.getCurrentPosition()/targetValue)/4));
             telemetry.update();
         }
@@ -35,20 +39,16 @@ public class DemoBot extends LinearOpMode { //drives forward 10 inches, strafes 
         backRight.setPower(0);
         backLeft.setPower(0);
 
-
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
-
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        while (backLeft.getCurrentPosition() < targetValue) {
-            frontRight.setPower(0.5);
+        while (Math.abs(backLeft.getCurrentPosition()) < targetValue) {
+            frontRight.setPower(-0.5);
             frontLeft.setPower(0.5);
             backRight.setPower(0.5);
-            backLeft.setPower(0.5);
-            telemetry.addData("current position: ", backLeft.getCurrentPosition());
-            telemetry.addData("percent done: ", (0.25+((backLeft.getCurrentPosition()/targetValue)/4)));
+            backLeft.setPower(-0.5);
+            telemetry.addData("current position: ", Math.abs(backLeft.getCurrentPosition()));
+            telemetry.addData("percent done: ", (0.25+((Math.abs(backLeft.getCurrentPosition())/targetValue)/4)));
             telemetry.update();
         }
 
@@ -57,19 +57,16 @@ public class DemoBot extends LinearOpMode { //drives forward 10 inches, strafes 
         backRight.setPower(0);
         backLeft.setPower(0);
 
-        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        while (backLeft.getCurrentPosition() < targetValue) {
-            frontRight.setPower(0.5);
-            frontLeft.setPower(0.5);
-            backRight.setPower(0.5);
-            backLeft.setPower(0.5);
-            telemetry.addData("current position: ", backLeft.getCurrentPosition());
-            telemetry.addData("percent done: ", (0.5+((backLeft.getCurrentPosition()/targetValue)/4)));
+        while (Math.abs(backLeft.getCurrentPosition()) < targetValue) {
+            frontRight.setPower(-0.5);
+            frontLeft.setPower(-0.5);
+            backRight.setPower(-0.5);
+            backLeft.setPower(-0.5);
+            telemetry.addData("current position: ", Math.abs(backLeft.getCurrentPosition()));
+            telemetry.addData("percent done: ", (0.5+((Math.abs(backLeft.getCurrentPosition())/targetValue)/4)));
             telemetry.update();
         }
 
@@ -78,19 +75,16 @@ public class DemoBot extends LinearOpMode { //drives forward 10 inches, strafes 
         backRight.setPower(0);
         backLeft.setPower(0);
 
-        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
-        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-
         backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        while (backLeft.getCurrentPosition() < targetValue) {
+        while (Math.abs(backLeft.getCurrentPosition()) < targetValue) {
             frontRight.setPower(0.5);
-            frontLeft.setPower(0.5);
-            backRight.setPower(0.5);
+            frontLeft.setPower(-0.5);
+            backRight.setPower(-0.5);
             backLeft.setPower(0.5);
-            telemetry.addData("current position: ", backLeft.getCurrentPosition());
-            telemetry.addData("percent done: ", (0.75+((backLeft.getCurrentPosition()/targetValue)/4)));
+            telemetry.addData("current position: ", Math.abs(backLeft.getCurrentPosition()));
+            telemetry.addData("percent done: ", (0.75+((Math.abs(backLeft.getCurrentPosition())/targetValue)/4)));
             telemetry.update();
         }
 
